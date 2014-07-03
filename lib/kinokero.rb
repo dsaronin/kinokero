f9feeea2a321eaf8f8d1a56796ba16449d1f799a
daudi amani <dsaronin@gmail.com>
2014-07-02 18:29:57 -0700

    refactoring - 5b control structures

--- lib/kinokero.rb
+++ lib/kinokero.rb
@@ -48,12 +48,67 @@ MIMETYPE_OAUTH =  "application/x-pkcs12"
 GCP_URL = 'https://www.google.com/'
 GCP_SERVICE = 'cloudprint'
 
+# -----------------------------------------------------------------------------
+
+# jingle constants required
+
+XMPP_SERVER     = "talk.google.com" 
+NS_GOOGLE_PUSH  = "google:push"
+GCP_CHANNEL     = "cloudprint.google.com"
+
+# -----------------------------------------------------------------------------
+
+
+# MY_PROXY_ID is a unique name for this running of the GCP connector client
+# formed with gem name + machine-node name (expected to be  unique)
+# TODO: make sure machine nodename is unique
+MY_PROXY_ID = "kinokero::"+`uname -n`.chop
+
+# CLIENT_NAME should be some string identifier for the client you are writing.
+CLIENT_NAME = MY_PROXY_ID + " cloudprint controller v0.0.1"
+
+
+
+# -----------------------------------------------------------------------------
+
 
   @@config = {
-    :default_temp        => "temp"   # placeholder for expansion
+
+      # class Proxy required
+    my_proxy_id:     MY_PROXY_ID,    # unique name for this running of the GCP connector client
+    client_name:     CLIENT_NAME,    # some string identifier for the client you are writing
+
+      # class Cloudprint required
+    mimetype_oauth:  MIMETYPE_OAUTH, # how to encoade oauth files
+    mimetype_ppd:    MIMETYPE_PPD,   # how to encode PPD files
+    polling_secs:    POLLING_SECS,   # secs to sleep before register polling again
+    truncate_log:    TRUNCATE_LOG,   # number of characters to truncate response logs 
+    followup_host:   FOLLOWUP_HOST,  #
+    followup_uri:    FOLLOWUP_URI,   #
+    gaia_host:       GAIA_HOST,      #
+    login_uri:       LOGIN_URI,      #
+    login_url:       LOGIN_URL,      #
+    gcp_url:         GCP_URL,        #
+    gcp_service:     GCP_SERVICE,    #
+
+    authorization_scope:         AUTHORIZATION_SCOPE,         #
+    authorization_redirect_uri:  AUTHORIZATION_REDIRECT_URI,  #
+    oauth2_token_endpoint:       OAUTH2_TOKEN_ENDPOINT,       #
+
+      # class Jingle required
+    xmpp_server:      XMPP_SERVER,     #  
+    ns_google_push:   NS_GOOGLE_PUSH,  #  
+    gcp_channel:      GCP_CHANNEL,     #  
+
+
+
   }
 
+# #########################################################################
+
   class_eval( "def config; @@config; end;" )
+
+# #########################################################################
 
 # these requires for kinokero modules must occur AFTER the above constants defined
 require "kinokero/cloudprint"
