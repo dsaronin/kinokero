27e6aae6ec321897f9b3182af3c057aa2660b245
daudi amani <dsaronin@gmail.com>
2014-07-16 17:33:08 -0700

    one-off fetch - 1f

--- lib/kinokero/cloudprint.rb
+++ lib/kinokero/cloudprint.rb
@@ -149,34 +149,6 @@ GCP_USER_ACTION_OTHER     = 100  # User has performed some other action
 
 # ------------------------------------------------------------------------------
 
-# gcp protocol to get the list of registered printers for the proxy
-#
-# * *Args*    :
-#   - 
-# * *Returns* :
-#   - 
-# * *Raises* :
-#   - 
-#
-  def self.gcp_get_printer_list(  )
-
-    list_response = Cloudprint.client_connection.post( ::Kinokero.gcp_service + GCP_LIST ) do |req|
-      # req.headers['Authorization'] = gcp_form_auth_token()  #  not required
-      req.body =  {
-        :proxy   => ::Kinokero.my_proxy_id
-      }
-
-      Kinokero::Log.log_request( 'get printer list', req )
-      
-    end  # request do
-    Kinokero::Log.log_response( 'get printer list', list_response )
-
-    return list_response.body
-
-  end
-
-# ------------------------------------------------------------------------------
-
 # handles the anonymous printer registration protocol
 #
 # * *Args*    :
@@ -714,6 +686,34 @@ GCP_USER_ACTION_OTHER     = 100  # User has performed some other action
 
   end
 
+
+# ------------------------------------------------------------------------------
+
+# gcp protocol to get the list of registered printers for the proxy
+#
+# * *Args*    :
+#   - 
+# * *Returns* :
+#   - 
+# * *Raises* :
+#   - 
+#
+  def gcp_get_printer_list(  )
+
+    list_response = Cloudprint.client_connection.post( ::Kinokero.gcp_service + GCP_LIST ) do |req|
+      req.headers['Authorization'] = gcp_form_auth_token()
+      req.body =  {
+        :proxy   => ::Kinokero.my_proxy_id
+      }
+
+      log_request( 'get printer list', req )
+      
+    end  # request do
+    log_response( 'get printer list', list_response )
+
+    return list_response.body
+
+  end
 
 # ------------------------------------------------------------------------------
 
