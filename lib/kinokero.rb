5621ddc55893560e6984257f8270aa565e4d6a8e
daudi amani <dsaronin@gmail.com>
2014-07-31 14:56:50 -0700

    ref json - 1b

--- lib/kinokero.rb
+++ lib/kinokero.rb
@@ -1,9 +1,9 @@
+require 'active_support/core_ext/module/attribute_accessors'
+
 require 'faraday-cookie_jar'
 require 'logger'
 require 'json'
 require 'forwardable'
-
-require 'active_support/core_ext/module/attribute_accessors'
 
 require "faraday"
 require "faraday_middleware"
