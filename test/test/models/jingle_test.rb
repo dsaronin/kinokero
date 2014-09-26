69eeb25f1eab40fde5aa8368d1c11b235f1c1ab8
daudi amani <dsaronin@gmail.com>
2014-09-26 00:20:53 -0700

    adding jingle/proxy scaffolding for tests

---
+++ test/test/models/jingle_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+require 'test_kinokero'
+
+class JingleTest < ActiveSupport::TestCase
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  setup    :setup_jig
+  teardown :teardown_jig
+
+  context "jingle" do   # test suite for printer.rb
+
+
+  end  # context jingle
+
+# _____________________________________________________________________________    
+
+end  # class test
---
+++ test/test/models/proxy_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+require 'test_kinokero'
+
+class ProxyTest < ActiveSupport::TestCase
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  setup    :setup_jig
+  teardown :teardown_jig
+
+  context "proxy" do   # test suite for printer.rb
+
+
+  end  # context proxy
+
+# _____________________________________________________________________________    
+
+end  # class test
